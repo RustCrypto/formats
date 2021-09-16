ee4bc34bba80ef11251bc00f6e1981ac702d9955
Tony Arcieri <bascule@gmail.com>
2021-09-16 12:08:34 -0600

    sec1 crate (#37)
    Adds a `sec1` crate similar to the `pkcs1` crate, but for EC keys as
    opposed to RSA keys, implementing the SEC1 `EcPrivateKey` ASN.1 DER
    schema:
    
    https://www.secg.org/sec1-v2.pdf
    
    This implementation was originally from the `elliptic-curve` crate,
    however extracting it here not only makes it reusable but makes possible
    a blanet impl of PKCS#8 support for any types which impl
    `FromEcPrivateKey`/`ToEcPrivateKey`, similar to the blanket impl
    provided for the `pkcs1` crate.
    
    It would eventually probably make sense to extract more of the SEC1
    encoding implementation into this crate from the `elliptic-curve` crate,
    namely support for the `Octet-String-to-Elliptic-Curve-Point` and
    `Elliptic-Curve-Point-to-Octet-String` conversions. However, they are
    omitted from an initial implementation for now.

---
+++ sec1/README.md
@@ -0,0 +1,48 @@
+# [RustCrypto]: SEC1 Elliptic Curve Cryptography Formats
+
+[![crate][crate-image]][crate-link]
+[![Docs][docs-image]][docs-link]
+[![Build Status][build-image]][build-link]
+![Apache2/MIT licensed][license-image]
+![Rust Version][rustc-image]
+[![Project Chat][chat-image]][chat-link]
+
+[Documentation][docs-link]
+
+## About
+
+Pure Rust implementation of [SEC1: Elliptic Curve Cryptography] encoding formats
+including ASN.1 DER-serialized private keys.
+
+## License
+
+Licensed under either of:
+
+ * [Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0)
+ * [MIT license](http://opensource.org/licenses/MIT)
+
+at your option.
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in the work by you, as defined in the Apache-2.0 license, shall be
+dual licensed as above, without any additional terms or conditions.
+
+[//]: # (badges)
+
+[crate-image]: https://img.shields.io/crates/v/sec1.svg
+[crate-link]: https://crates.io/crates/sec1
+[docs-image]: https://docs.rs/sec1/badge.svg
+[docs-link]: https://docs.rs/sec1/
+[license-image]: https://img.shields.io/badge/license-Apache2.0/MIT-blue.svg
+[rustc-image]: https://img.shields.io/badge/rustc-1.51+-blue.svg
+[chat-image]: https://img.shields.io/badge/zulip-join_chat-blue.svg
+[chat-link]: https://rustcrypto.zulipchat.com/#narrow/stream/300570-formats
+[build-image]: https://github.com/RustCrypto/formats/workflows/sec1/badge.svg?branch=master&event=push
+[build-link]: https://github.com/RustCrypto/formats/actions
+
+[//]: # (links)
+
+[RustCrypto]: https://github.com/rustcrypto
+[SEC1: Elliptic Curve Cryptography]: https://www.secg.org/sec1-v2.pdf
---
+++ sec1/src/private_key.rs
@@ -0,0 +1,145 @@
+//! SEC1 elliptic curve private key support.
+//!
+//! Support for ASN.1 DER-encoded elliptic curve private keys as described in
+//! SEC1: Elliptic Curve Cryptography (Version 2.0) Appendix C.4 (p.108):
+//!
+//! <https://www.secg.org/sec1-v2.pdf>
+
+#[cfg(feature = "alloc")]
+pub(crate) mod document;
+
+use crate::{EcParameters, Error};
+use core::{
+    convert::{TryFrom, TryInto},
+    fmt,
+};
+use der::{
+    asn1::{Any, BitString, ContextSpecific, OctetString},
+    Decodable, Encodable, Message, TagNumber,
+};
+
+/// Type label for PEM-encoded private keys.
+#[cfg(feature = "pem")]
+pub(crate) const PEM_TYPE_LABEL: &str = "EC PRIVATE KEY";
+
+/// `ECPrivateKey` version.
+///
+/// From RFC5913 Section 3:
+/// > version specifies the syntax version number of the elliptic curve
+/// > private key structure.  For this version of the document, it SHALL
+/// > be set to ecPrivkeyVer1, which is of type INTEGER and whose value
+/// > is one (1).
+const VERSION: u8 = 1;
+
+/// Context-specific tag number for the elliptic curve parameters.
+const EC_PARAMETERS_TAG: TagNumber = TagNumber::new(0);
+
+/// Context-specific tag number for the public key.
+const PUBLIC_KEY_TAG: TagNumber = TagNumber::new(1);
+
+/// SEC1 elliptic curve private key.
+///
+/// Described in [SEC1: Elliptic Curve Cryptography (Version 2.0)]
+/// Appendix C.4 (p.108) and also [RFC5915 Section 3]:
+///
+/// ```text
+/// ECPrivateKey ::= SEQUENCE {
+///   version        INTEGER { ecPrivkeyVer1(1) } (ecPrivkeyVer1),
+///   privateKey     OCTET STRING,
+///   parameters [0] ECParameters {{ NamedCurve }} OPTIONAL,
+///   publicKey  [1] BIT STRING OPTIONAL
+/// }
+/// ```
+///
+/// When encoded as PEM (text), keys in this format begin with the following:
+///
+/// ```text
+/// -----BEGIN EC PRIVATE KEY-----
+/// ```
+///
+/// [SEC1: Elliptic Curve Cryptography (Version 2.0)]: https://www.secg.org/sec1-v2.pdf
+/// [RFC5915 Section 3]: https://datatracker.ietf.org/doc/html/rfc5915#section-3
+#[derive(Clone)]
+pub struct EcPrivateKey<'a> {
+    /// Private key data.
+    pub private_key: &'a [u8],
+
+    /// Elliptic curve parameters.
+    pub parameters: Option<EcParameters>,
+
+    /// Public key data, optionally available if version is V2.
+    pub public_key: Option<&'a [u8]>,
+}
+
+impl<'a> TryFrom<&'a [u8]> for EcPrivateKey<'a> {
+    type Error = Error;
+
+    fn try_from(bytes: &'a [u8]) -> Result<EcPrivateKey<'a>, Error> {
+        Ok(Self::from_der(bytes)?)
+    }
+}
+
+impl<'a> TryFrom<Any<'a>> for EcPrivateKey<'a> {
+    type Error = der::Error;
+
+    fn try_from(any: Any<'a>) -> der::Result<EcPrivateKey<'a>> {
+        any.sequence(|decoder| {
+            if decoder.uint8()? != VERSION {
+                return Err(der::Tag::Integer.value_error());
+            }
+
+            let private_key = decoder.octet_string()?.as_bytes();
+
+            let parameters = decoder
+                .context_specific(EC_PARAMETERS_TAG)?
+                .map(TryInto::try_into)
+                .transpose()?;
+
+            let public_key = decoder
+                .context_specific(PUBLIC_KEY_TAG)?
+                .map(|any| any.bit_string())
+                .transpose()?
+                .map(|bs| bs.as_bytes());
+
+            Ok(EcPrivateKey {
+                private_key,
+                parameters,
+                public_key,
+            })
+        })
+    }
+}
+
+impl<'a> Message<'a> for EcPrivateKey<'a> {
+    fn fields<F, T>(&self, f: F) -> der::Result<T>
+    where
+        F: FnOnce(&[&dyn Encodable]) -> der::Result<T>,
+    {
+        f(&[
+            &VERSION,
+            &OctetString::new(self.private_key)?,
+            &self.parameters.as_ref().map(|params| ContextSpecific {
+                tag_number: EC_PARAMETERS_TAG,
+                value: params.into(),
+            }),
+            &self
+                .public_key
+                .map(|pk| {
+                    BitString::new(pk).map(|value| ContextSpecific {
+                        tag_number: PUBLIC_KEY_TAG,
+                        value: value.into(),
+                    })
+                })
+                .transpose()?,
+        ])
+    }
+}
+
+impl<'a> fmt::Debug for EcPrivateKey<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("EcPrivateKey")
+            .field("parameters", &self.parameters)
+            .field("public_key", &self.public_key)
+            .finish() // TODO: use `finish_non_exhaustive` when stable
+    }
+}
