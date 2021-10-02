740fa7366e0975285bd1bec8fe92fc3fee4ba099
Tony Arcieri <bascule@gmail.com>
2021-10-02 11:26:19 -0600

    der: remove accidentally checked in target directory
    Unfortunately `cross` leaves these target directories around when used
    within a workspace, and it was accidentally included in the initial
    import of the source code.

--- der/target/aarch64-unknown-linux-gnu/release/deps/derive-04c64e94a2476b06.d
+++
@@ -1,5 +0,0 @@
-/target/aarch64-unknown-linux-gnu/release/deps/derive-04c64e94a2476b06: tests/derive.rs
-
-/target/aarch64-unknown-linux-gnu/release/deps/derive-04c64e94a2476b06.d: tests/derive.rs
-
-tests/derive.rs:
