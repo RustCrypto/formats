74742f5691a6ec10a04e0bc8e7f774f754137ca4
Carl Wallace <carl@redhoundsoftware.com>
2021-10-09 15:38:36 -0400

    x509: add encoder/decoder for Validity structure (#81)

---
+++ x509/src/validity.rs
@@ -0,0 +1,30 @@
+//! Validity [`Validity`] as defined in RFC 5280
+
+use crate::Time;
+use core::convert::TryFrom;
+use der::{Decodable, Error, Message, Result};
+
+/// X.509 `Validity` as defined in [RFC 5280 Section 4.1.2.5]
+///
+/// ```text
+/// Validity ::= SEQUENCE {
+///     notBefore      Time,
+///     notAfter       Time  }
+/// ```
+/// [RFC 5280 Section 4.1.2.5]: https://datatracker.ietf.org/doc/html/rfc5280#section-4.1.2.5
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Message)]
+pub struct Validity {
+    /// notBefore value
+    pub not_before: Time,
+
+    /// notAfter value
+    pub not_after: Time,
+}
+
+impl<'a> TryFrom<&'a [u8]> for Validity {
+    type Error = Error;
+
+    fn try_from(bytes: &'a [u8]) -> Result<Self> {
+        Self::from_der(bytes)
+    }
+}
