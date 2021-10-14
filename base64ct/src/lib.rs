c4982b74e21e7e3d0ca1101cc5e37eeb7fc0fd07
Tony Arcieri <bascule@gmail.com>
2021-10-14 15:55:26 -0600

    base64ct v1.1.1 (#104)

--- base64ct/src/lib.rs
+++ base64ct/src/lib.rs
@@ -84,7 +84,7 @@
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
     html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
-    html_root_url = "https://docs.rs/base64ct/1.1.0"
+    html_root_url = "https://docs.rs/base64ct/1.1.1"
 )]
 #![warn(missing_docs, rust_2018_idioms)]
 
