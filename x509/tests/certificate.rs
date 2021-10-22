13d6041fe3bdeb956e15985cb6443883c4dddfee
Carl Wallace <crwallace1970@gmail.com>
2021-10-22 14:04:51 -0400

    rustfmt

--- x509/tests/certificate.rs
+++ x509/tests/certificate.rs
@@ -2,12 +2,11 @@
 use der::asn1::UIntBytes;
 use der::{Decodable, Tag};
 use hex_literal::hex;
-use x509::{
-    BasicConstraints, Certificate, CertificatePolicies, DeferCertificate,
-    SubjectKeyIdentifier,
-};
 #[cfg(feature = "alloc")]
 use x509::KeyUsage;
+use x509::{
+    BasicConstraints, Certificate, CertificatePolicies, DeferCertificate, SubjectKeyIdentifier,
+};
 
 ///   TBSCertificate  ::=  SEQUENCE  {
 ///       version         [0]  Version DEFAULT v1,
