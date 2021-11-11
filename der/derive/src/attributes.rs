0a601d57f0b40c91ab42b26a1eb10a8376d2a40c
Carl Wallace <crwallace1970@gmail.com>
2021-11-11 12:58:05 -0500

    recommended change

--- der/derive/src/attributes.rs
+++ der/derive/src/attributes.rs
@@ -119,9 +119,9 @@ impl FieldAttrs {
                 }
 
                 tag_mode = Some(mode);
-            } else if let Some(_default_attr_str) = attr.parse_value::<String>("default") {
+            } else if attr.parse_value::<String>("default").is_some() {
                 if default.is_some() {
-                    abort!(attr.value, "duplicate ASN.1 `tag_mode` attribute");
+                    abort!(attr.value, "duplicate ASN.1 `default` attribute");
                 }
 
                 default = Some(attr.lit_str.parse::<Path>().unwrap());
