4ca38533e6ae97b05486e34653b5dcee0b7c2e02
Carl Wallace <crwallace1970@gmail.com>
2021-11-11 12:18:30 -0500

    rustfmt

--- der/derive/src/attributes.rs
+++ der/derive/src/attributes.rs
@@ -232,7 +232,7 @@ impl AttrNameValue {
                     })) => out.push(Self {
                         name: path.clone(),
                         value: lit_str.value(),
-                        lit_str: lit_str.clone()
+                        lit_str: lit_str.clone(),
                     }),
                     _ => abort!(nested, "malformed `asn1` attribute"),
                 }
