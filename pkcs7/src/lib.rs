88beff88e2602c46e9c74b5619de8927610b5bd7
Tony Arcieri <bascule@gmail.com>
2021-11-15 13:06:01 -0700

    pkcs7 v0.3.0 (#225)

--- pkcs7/src/lib.rs
+++ pkcs7/src/lib.rs
@@ -4,7 +4,7 @@
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
     html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
-    html_root_url = "https://docs.rs/pkcs7/0.0.0"
+    html_root_url = "https://docs.rs/pkcs7/0.3.0"
 )]
 #![forbid(unsafe_code, clippy::unwrap_used)]
 #![warn(missing_docs, rust_2018_idioms, unused_qualifications)]
