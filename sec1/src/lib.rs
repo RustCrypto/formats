98e2383913f71f1a1a02796dd5c195c0048f789e
Tony Arcieri <bascule@gmail.com>
2021-11-17 06:57:02 -0700

    sec1 v0.2.0

--- sec1/src/lib.rs
+++ sec1/src/lib.rs
@@ -4,7 +4,7 @@
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
     html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
-    html_root_url = "https://docs.rs/sec1/0.2.0-pre"
+    html_root_url = "https://docs.rs/sec1/0.2.0"
 )]
 #![forbid(unsafe_code, clippy::unwrap_used)]
 #![warn(missing_docs, rust_2018_idioms, unused_qualifications)]
