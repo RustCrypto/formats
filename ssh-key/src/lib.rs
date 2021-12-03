2339999572935489c027909162ae0063e2f67ed3
Tony Arcieri <bascule@gmail.com>
2021-12-02 18:36:11 -0700

    ssh-key v0.1.0 (#274)

--- ssh-key/src/lib.rs
+++ ssh-key/src/lib.rs
@@ -38,7 +38,7 @@
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
     html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
-    html_root_url = "https://docs.rs/ssh-key/0.0.0"
+    html_root_url = "https://docs.rs/ssh-key/0.1.0"
 )]
 #![forbid(unsafe_code, clippy::unwrap_used)]
 #![warn(missing_docs, rust_2018_idioms, unused_qualifications)]
