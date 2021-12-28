620f8de6bf693d9bf227fef2a28c874a6851b7f6
Tony Arcieri <bascule@gmail.com>
2021-12-28 16:41:16 -0700

    base64ct v1.3.3

--- base64ct/src/lib.rs
+++ base64ct/src/lib.rs
@@ -4,7 +4,7 @@
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
     html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
-    html_root_url = "https://docs.rs/base64ct/1.3.2"
+    html_root_url = "https://docs.rs/base64ct/1.3.3"
 )]
 #![doc = include_str!("../README.md")]
 
