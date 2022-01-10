5a53239be50375f487d573675379c3f696d443c2
Nathaniel McCallum <nathaniel@profian.com>
2022-01-09 22:50:27 -0500

    Add a PKCS#10 (certification requests) implementation
    This crate adds data structures for requesting a certificate for a public key.
    The most notable type is `CertReq` since it is widely used in X.509 contexts.
    
    Signed-off-by: Nathaniel McCallum <nathaniel@profian.com>

---
+++ pkcs10/src/info.rs
@@ -0,0 +1,38 @@
+use super::{Attributes, Version};
+use der::{Decodable, Sequence};
+
+/// PKCS#10 `CertificationRequestInfo` as defined in [RFC 2986 Section 4].
+///
+/// ```text
+/// CertificationRequestInfo ::= SEQUENCE {
+///     version       INTEGER { v1(0) } (v1,...),
+///     subject       Name,
+///     subjectPKInfo SubjectPublicKeyInfo{{ PKInfoAlgorithms }},
+///     attributes    [0] Attributes{{ CRIAttributes }}
+/// }
+/// ```
+///
+/// [RFC 2986 Section 4]: https://datatracker.ietf.org/doc/html/rfc2986#section-4
+#[derive(Clone, Debug, PartialEq, Eq, Sequence)]
+pub struct CertReqInfo<'a> {
+    /// Certification request version.
+    pub version: Version,
+
+    /// Subject name.
+    pub subject: x509::Name<'a>,
+
+    /// Subject public key info.
+    pub public_key: spki::SubjectPublicKeyInfo<'a>,
+
+    /// Request attributes.
+    #[asn1(context_specific = "0", tag_mode = "IMPLICIT")]
+    pub attributes: Attributes<'a>,
+}
+
+impl<'a> TryFrom<&'a [u8]> for CertReqInfo<'a> {
+    type Error = der::Error;
+
+    fn try_from(bytes: &'a [u8]) -> Result<Self, Self::Error> {
+        Self::from_der(bytes)
+    }
+}
