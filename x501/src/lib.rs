63dd56c58b138b5eddd74ab5dd7663b4f57856fa
Tony Arcieri <bascule@gmail.com>
2022-02-04 14:21:47 -0700

    x501 v0.1.0-pre.0

--- x501/src/lib.rs
+++ x501/src/lib.rs
@@ -4,7 +4,7 @@
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
     html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
-    html_root_url = "https://docs.rs/x501/0.0.0"
+    html_root_url = "https://docs.rs/x501/0.1.0-pre.0"
 )]
 #![forbid(unsafe_code)]
 #![warn(missing_docs, rust_2018_idioms)]
