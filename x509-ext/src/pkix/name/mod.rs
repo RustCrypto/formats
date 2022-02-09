575fa195759eae8afe604afc7b45e952c3c34ac5
Nathaniel McCallum <nathaniel@profian.com>
2022-02-08 19:03:32 -0500

    x509-ext: add EdiPartyName
    Signed-off-by: Nathaniel McCallum <nathaniel@profian.com>

--- x509-ext/src/pkix/name/mod.rs
+++ x509-ext/src/pkix/name/mod.rs
@@ -3,5 +3,7 @@
 //! [RFC 5280 Section 4.2.1.6]: https://datatracker.ietf.org/doc/html/rfc5280#section-4.2.1.6
 
 mod dirstr;
+mod ediparty;
 
 pub use dirstr::DirectoryString;
+pub use ediparty::EdiPartyName;
