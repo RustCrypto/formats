3ea6df0d5e5b8a2320c5392f80bf7659e0826ab4
Nathaniel McCallum <nathaniel@profian.com>
2022-02-11 16:50:48 -0500

    pkcs1: derive(Structure) for OtherPrimeInfo
    Signed-off-by: Nathaniel McCallum <nathaniel@profian.com>

--- pkcs1/src/private_key/other_prime_info.rs
+++ pkcs1/src/private_key/other_prime_info.rs
@@ -15,7 +15,7 @@ use der::{asn1::UIntBytes, Decodable, Decoder, Encodable, Sequence};
 /// ```
 ///
 /// [RFC 8017 Appendix 1.2]: https://datatracker.ietf.org/doc/html/rfc8017#appendix-A.1.2
-#[derive(Clone)]
+#[derive(Clone, Sequence)]
 #[cfg_attr(docsrs, doc(cfg(feature = "alloc")))]
 pub struct OtherPrimeInfo<'a> {
     /// Prime factor `r_i` of `n`, where `i` >= 3.
@@ -26,25 +26,4 @@ pub struct OtherPrimeInfo<'a> {
 
     /// CRT coefficient: `t_i = (r_1 * r_2 * ... * r_(i-1))^(-1) mod r_i`.
     pub coefficient: UIntBytes<'a>,
-}
-
-impl<'a> Decodable<'a> for OtherPrimeInfo<'a> {
-    fn decode(decoder: &mut Decoder<'a>) -> der::Result<Self> {
-        decoder.sequence(|decoder| {
-            Ok(Self {
-                prime: decoder.decode()?,
-                exponent: decoder.decode()?,
-                coefficient: decoder.decode()?,
-            })
-        })
-    }
-}
-
-impl<'a> Sequence<'a> for OtherPrimeInfo<'a> {
-    fn fields<F, T>(&self, f: F) -> der::Result<T>
-    where
-        F: FnOnce(&[&dyn Encodable]) -> der::Result<T>,
-    {
-        f(&[&self.prime, &self.exponent, &self.coefficient])
-    }
 }
