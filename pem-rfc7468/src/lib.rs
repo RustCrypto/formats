1d30cc210fa1156beff05379b0a0493e4902a40f
bstrie <865233+bstrie@users.noreply.github.com>
2022-03-11 20:58:44 -0500

    chore: bump crate prerelease versions (#493)
    This bumps the prerelease versions for the following crates:
    
    base64ct
    const-oid
    der
    der_derive
    pem-rfc7468
    spki

--- pem-rfc7468/src/lib.rs
+++ pem-rfc7468/src/lib.rs
@@ -3,7 +3,7 @@
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
     html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
-    html_root_url = "https://docs.rs/pem-rfc7468/0.4.0-pre.0"
+    html_root_url = "https://docs.rs/pem-rfc7468/0.4.0-pre.1"
 )]
 #![forbid(unsafe_code, clippy::unwrap_used)]
 #![warn(missing_docs, rust_2018_idioms, unused_qualifications)]
