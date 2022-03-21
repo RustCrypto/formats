b32f365615cfc3bc9522dcfb0d4a626f322d9649
Carl Wallace <crwallace1970@gmail.com>
2022-03-21 16:09:42 -0400

    add OCSP encoders and decoders

---
+++ ocsp/src/lib.rs
@@ -0,0 +1,6 @@
+//! The ocsp module features encoders and decoders for the structures defined in [RFC 6960](https://datatracker.ietf.org/doc/html/rfc6960).
+#![no_std]
+
+pub mod ocsp;
+
+extern crate alloc;
