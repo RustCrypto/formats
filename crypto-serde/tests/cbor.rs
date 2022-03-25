fbfaa13038f91d4251b3410a0e29fb28b3f29da2
Tony Arcieri <bascule@gmail.com>
2022-03-25 15:10:46 -0600

    crypto-serde: proptests (#532)
    Adds initial proptests which ensure that randomly generated inputs round
    trip back to their original values

--- crypto-serde/tests/cbor.rs
+++ crypto-serde/tests/cbor.rs
@@ -3,6 +3,7 @@
 use ciborium::{de, ser};
 use crypto_serde::HexUpperOrBin;
 use hex_literal::hex;
+use proptest::{prelude::*, string::*};
 
 /// Example input to be serialized.
 const EXAMPLE_BYTES: &[u8] = &hex!("000102030405060708090A0B0C0D0E0F");
@@ -21,4 +22,15 @@ fn serialize() {
     let mut serialized = Vec::new();
     ser::into_writer(&HexUpperOrBin::from(EXAMPLE_BYTES), &mut serialized).unwrap();
     assert_eq!(&serialized, CBOR_BYTES);
+}
+
+proptest! {
+    #[test]
+    fn round_trip(bytes in bytes_regex(".{0,256}").unwrap()) {
+        let mut serialized = Vec::new();
+        ser::into_writer(&HexUpperOrBin::from(bytes.as_ref()), &mut serialized).unwrap();
+
+        let deserialized = de::from_reader::<HexUpperOrBin, _>(serialized.as_slice()).unwrap();
+        prop_assert_eq!(bytes, deserialized.0);
+    }
 }
