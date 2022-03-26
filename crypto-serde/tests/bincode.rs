a4e63925cf1f279fa86390447ed6d54712a5d3b3
dAxpeDDa <daxpedda@gmail.com>
2022-03-26 23:00:30 +0100

    Test bincode array

--- crypto-serde/tests/bincode.rs
+++ crypto-serde/tests/bincode.rs
@@ -5,23 +5,71 @@
 use crypto_serde::HexUpperOrBin;
 use hex_literal::hex;
 use proptest::{prelude::*, string::*};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
 /// Example input to be serialized.
-const EXAMPLE_BYTES: &[u8] = &hex!("000102030405060708090A0B0C0D0E0F");
+const EXAMPLE_BYTES: [u8; 16] = hex!("000102030405060708090A0B0C0D0E0F");
 
-/// bincode serialization of [`EXAMPLE_BYTES`].
-const BINCODE_BYTES: &[u8] = &hex!("1000000000000000000102030405060708090A0B0C0D0E0F");
+/// bincode serialization of [`EXAMPLE_BYTES`] as a slice.
+const BINCODE_SLICE: [u8; 24] = hex!("1000000000000000000102030405060708090A0B0C0D0E0F");
+
+/// bincode serialization of [`EXAMPLE_BYTES`] as an array.
+const BINCODE_ARRAY: [u8; 16] = EXAMPLE_BYTES;
 
 #[test]
 fn deserialize() {
-    let deserialized = bincode::deserialize::<HexUpperOrBin>(BINCODE_BYTES).unwrap();
+    let deserialized = bincode::deserialize::<HexUpperOrBin>(&BINCODE_SLICE).unwrap();
     assert_eq!(deserialized.as_ref(), EXAMPLE_BYTES);
 }
 
 #[test]
+fn deserialize_owned() {
+    let deserialized =
+        bincode::deserialize_from::<_, HexUpperOrBin>(BINCODE_SLICE.as_slice()).unwrap();
+    assert_eq!(deserialized.as_ref(), EXAMPLE_BYTES);
+}
+
+#[test]
+fn deserialize_array() {
+    struct Test([u8; 16]);
+
+    impl<'de> Deserialize<'de> for Test {
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: Deserializer<'de>,
+        {
+            crypto_serde::deserialize_array_hex_or_bin::<16, _>(deserializer).map(Self)
+        }
+    }
+
+    let deserialized = bincode::deserialize::<Test>(&BINCODE_ARRAY).unwrap();
+    assert_eq!(deserialized.0, EXAMPLE_BYTES);
+
+    let deserialized = bincode::deserialize_from::<_, Test>(BINCODE_ARRAY.as_slice()).unwrap();
+    assert_eq!(deserialized.0, EXAMPLE_BYTES);
+}
+
+#[test]
 fn serialize() {
-    let serialized = bincode::serialize(&HexUpperOrBin::from(EXAMPLE_BYTES)).unwrap();
-    assert_eq!(&serialized, BINCODE_BYTES);
+    let serialized = bincode::serialize(&HexUpperOrBin::from(EXAMPLE_BYTES.as_slice())).unwrap();
+    assert_eq!(&serialized, &BINCODE_SLICE);
+}
+
+#[test]
+fn serialize_array() {
+    struct Test([u8; 16]);
+
+    impl Serialize for Test {
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+        {
+            crypto_serde::serialize_array_hex_upper_or_bin(&self.0, serializer)
+        }
+    }
+
+    let serialized = bincode::serialize(&Test(EXAMPLE_BYTES)).unwrap();
+    assert_eq!(&serialized, &BINCODE_ARRAY);
 }
 
 proptest! {
