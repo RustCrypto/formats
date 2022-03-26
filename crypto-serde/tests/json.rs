b2178cb40a9d0268fc9cebabbb34f8ae6cbd1c82
dAxpeDDa <daxpedda@gmail.com>
2022-03-26 23:38:51 +0100

    Test serde_json array

--- crypto-serde/tests/json.rs
+++ crypto-serde/tests/json.rs
@@ -5,10 +5,11 @@
 use crypto_serde::{HexLowerOrBin, HexUpperOrBin};
 use hex_literal::hex;
 use proptest::{prelude::*, string::*};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use serde_json as json;
 
 /// Example input to be serialized.
-const EXAMPLE_BYTES: &[u8] = &hex!("000102030405060708090A0B0C0D0E0F");
+const EXAMPLE_BYTES: [u8; 16] = hex!("000102030405060708090A0B0C0D0E0F");
 
 /// Lower-case hex serialization of [`EXAMPLE_BYTES`].
 const HEX_LOWER: &str = "\"000102030405060708090a0b0c0d0e0f\"";
@@ -18,7 +19,7 @@ const HEX_UPPER: &str = "\"000102030405060708090A0B0C0D0E0F\"";
 
 #[test]
 fn hex_lower() {
-    let serialized = json::to_string(&HexLowerOrBin::from(EXAMPLE_BYTES)).unwrap();
+    let serialized = json::to_string(&HexLowerOrBin::from(EXAMPLE_BYTES.as_slice())).unwrap();
     assert_eq!(serialized, HEX_LOWER);
 
     let deserialized = json::from_str::<HexLowerOrBin>(&serialized).unwrap();
@@ -27,11 +28,40 @@ fn hex_lower() {
 
 #[test]
 fn hex_upper() {
-    let serialized = json::to_string(&HexUpperOrBin::from(EXAMPLE_BYTES)).unwrap();
+    let serialized = json::to_string(&HexUpperOrBin::from(EXAMPLE_BYTES.as_slice())).unwrap();
     assert_eq!(serialized, HEX_UPPER);
 
     let deserialized = json::from_str::<HexUpperOrBin>(&serialized).unwrap();
     assert_eq!(deserialized.as_ref(), EXAMPLE_BYTES);
+}
+
+#[test]
+fn array() {
+    struct Test([u8; 16]);
+
+    impl<'de> Deserialize<'de> for Test {
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: Deserializer<'de>,
+        {
+            crypto_serde::deserialize_array_hex_or_bin::<16, _>(deserializer).map(Self)
+        }
+    }
+
+    impl Serialize for Test {
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+        {
+            crypto_serde::serialize_array_hex_lower_or_bin(&self.0, serializer)
+        }
+    }
+
+    let serialized = json::to_string(&Test(EXAMPLE_BYTES)).unwrap();
+    assert_eq!(serialized, HEX_LOWER);
+
+    let deserialized = json::from_str::<Test>(&serialized).unwrap();
+    assert_eq!(deserialized.0, EXAMPLE_BYTES);
 }
 
 proptest! {
