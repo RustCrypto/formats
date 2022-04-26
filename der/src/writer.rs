f0a3d96b384b8b0472dcfe427acdeccd52edfbea
Tony Arcieri <bascule@gmail.com>
2022-04-25 19:05:59 -0600

    der: blanket impl `Writer` for `T: std::io::Write` (#617)

--- der/src/writer.rs
+++ der/src/writer.rs
@@ -2,6 +2,9 @@
 
 use crate::Result;
 
+#[cfg(feature = "std")]
+use std::io;
+
 /// Writer trait which outputs encoded DER.
 pub trait Writer {
     /// Write the given DER-encoded bytes as output.
@@ -10,5 +13,14 @@ pub trait Writer {
     /// Write a single byte.
     fn write_byte(&mut self, byte: u8) -> Result<()> {
         self.write(&[byte])
+    }
+}
+
+#[cfg(feature = "std")]
+#[cfg_attr(docsrs, doc(cfg(feature = "std")))]
+impl<W: io::Write> Writer for W {
+    fn write(&mut self, slice: &[u8]) -> Result<()> {
+        <Self as io::Write>::write(self, slice)?;
+        Ok(())
     }
 }
