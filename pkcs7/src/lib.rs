b607a77d2cb43694f513cae8a63cf79be1f70805
Nicholas Bishop <nbishop@nbishop.net>
2022-08-21 21:39:27 -0400

    pkcs7: fix duplicate readme inclusion

--- pkcs7/src/lib.rs
+++ pkcs7/src/lib.rs
@@ -1,7 +1,6 @@
 #![no_std]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![doc = include_str!("../README.md")]
-#![doc = include_str!("../README.md")]
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
     html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
