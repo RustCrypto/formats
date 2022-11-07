dc735cd41dce09dfbcd5c86eed8307cf2b4f5df3
Tony Arcieri <bascule@gmail.com>
2022-11-06 19:20:49 -0700

    pkcs12: initial crate
    Initial boilerplate for a PKCS#12 crate

---
+++ pkcs12/src/lib.rs
@@ -0,0 +1,9 @@
+#![no_std]
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![doc = include_str!("../README.md")]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/RustCrypto/media/6ee8e381/logo.svg",
+    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/6ee8e381/logo.svg"
+)]
+#![forbid(unsafe_code, clippy::unwrap_used)]
+#![warn(missing_docs, rust_2018_idioms, unused_qualifications)]
