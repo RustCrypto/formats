1f79f77d9930296f07245fd9c7ffa74fefae89bd
Tony Arcieri <bascule@gmail.com>
2022-11-12 14:14:16 -0700

    const-oid: add `DynAssociatedOid` trait
    Adds an object safe value-dependent trait which returns an
    `ObjectIdentifier` dynamically, with a blanket impl for types which impl
    `AssociatedOid`.

--- const-oid/src/lib.rs
+++ const-oid/src/lib.rs
@@ -46,6 +46,22 @@ pub trait AssociatedOid {
     const OID: ObjectIdentifier;
 }
 
+/// A trait which associates a dynamic, `&self`-dependent OID with a type,
+/// which may change depending on the type's value.
+///
+/// This trait is object safe and auto-impl'd for any types which impl
+/// [`AssociatedOid`].
+pub trait DynAssociatedOid {
+    /// Get the OID associated with this value.
+    fn oid(&self) -> ObjectIdentifier;
+}
+
+impl<T: AssociatedOid> DynAssociatedOid for T {
+    fn oid(&self) -> ObjectIdentifier {
+        T::OID
+    }
+}
+
 /// Object identifier (OID).
 ///
 /// OIDs are hierarchical structures consisting of "arcs", i.e. integer
