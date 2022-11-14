bc4f16198ef63a204e0ec9e10e2a348585988986
Damian Poddebniak <poddebniak@mailbox.org>
2022-11-14 13:58:34 +0100

    test: Use `trybuild` crate to test non-compiling code.

--- tls_codec/derive/tests/attributes.rs
+++ tls_codec/derive/tests/attributes.rs
@@ -1,40 +1,5 @@
 #[test]
-fn that_duplicate_skip_attributes_dont_compile() {
-    // TODO: It works. But how to test this?
-    // use tls_codec_derive::{TlsDeserialize, TlsSerialize, TlsSize};
-    //
-    // #[derive(TlsDeserialize, TlsSerialize, TlsSize)]
-    // struct StructWithDuplicateSkip {
-    //     #[tls_codec(skip, skip)]
-    //     a: u8,
-    // }
-}
-
-#[test]
-fn that_skip_attribute_does_not_compile_on_enums() {
-    // TODO: It works. But how to test this?
-    // use tls_codec_derive::{TlsDeserialize, TlsSerialize, TlsSize};
-    //
-    // #[derive(TlsDeserialize, TlsSerialize, TlsSize)]
-    // #[repr(u8)]
-    // enum EnumWithSkip {
-    //     #[tls_codec(skip)]
-    //     A,
-    //     B,
-    // }
-}
-
-#[test]
-fn that_non_default_field_does_not_compile() {
-    // TODO: It works. But how to test this?
-    // use tls_codec_derive::{TlsDeserialize, TlsSerialize, TlsSize};
-    //
-    // struct NonDefaultField {}
-    //
-    // #[derive(TlsDeserialize, TlsSerialize, TlsSize)]
-    // struct StructWithNonDefaultField {
-    //     #[tls_codec(skip)]
-    //     a: NonDefaultField,
-    //     b: u8,
-    // }
+fn attributes() {
+    let t = trybuild::TestCases::new();
+    t.compile_fail("tests/attributes/*.rs");
 }
