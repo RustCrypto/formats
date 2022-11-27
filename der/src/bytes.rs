32cc65736a1189c80521d0ada216cae4389dac1e
Arthur Gautier <arthur.gautier@arista.com>
2022-11-27 12:45:07 -0800

    der: bytes are immutable Vec isn't needed

--- der/src/bytes.rs
+++ der/src/bytes.rs
@@ -1,30 +1,30 @@
-//! Common handling for types backed by byte slices with enforcement of a
+//! Common handling for types backed by byte allocation with enforcement of a
 //! library-level length limitation i.e. `Length::max()`.
 
 use crate::{
     str_slice::StrSlice, DecodeValue, DerOrd, EncodeValue, Error, Header, Length, Reader, Result,
     Writer,
 };
-use alloc::vec::Vec;
+use alloc::boxed::Box;
 use core::cmp::Ordering;
 
 /// Byte slice newtype which respects the `Length::max()` limit.
 #[derive(Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
-pub(crate) struct ByteVec {
+pub(crate) struct Bytes {
     /// Precomputed `Length` (avoids possible panicking conversions)
     length: Length,
 
     /// Inner value
-    inner: Vec<u8>,
+    inner: Box<[u8]>,
 }
 
-impl ByteVec {
-    /// Create a new [`ByteVec`], ensuring that the provided `slice` value
+impl Bytes {
+    /// Create a new [`Bytes`], ensuring that the provided `slice` value
     /// is shorter than `Length::max()`.
     pub fn new(slice: &[u8]) -> Result<Self> {
         Ok(Self {
             length: Length::try_from(slice.len())?,
-            inner: Vec::from(slice),
+            inner: Box::from(slice),
         })
     }
 
@@ -39,19 +39,19 @@ impl ByteVec {
     }
 }
 
-impl AsRef<[u8]> for ByteVec {
+impl AsRef<[u8]> for Bytes {
     fn as_ref(&self) -> &[u8] {
         self.as_slice()
     }
 }
 
-impl<'a> DecodeValue<'a> for ByteVec {
+impl<'a> DecodeValue<'a> for Bytes {
     fn decode_value<R: Reader<'a>>(reader: &mut R, header: Header) -> Result<Self> {
         reader.read_slice(header.length).and_then(Self::new)
     }
 }
 
-impl EncodeValue for ByteVec {
+impl EncodeValue for Bytes {
     fn value_len(&self) -> Result<Length> {
         Ok(self.length)
     }
@@ -61,43 +61,43 @@ impl EncodeValue for ByteVec {
     }
 }
 
-impl Default for ByteVec {
+impl Default for Bytes {
     fn default() -> Self {
         Self {
             length: Length::ZERO,
-            inner: Vec::new(),
+            inner: Box::new([]),
         }
     }
 }
 
-impl DerOrd for ByteVec {
+impl DerOrd for Bytes {
     fn der_cmp(&self, other: &Self) -> Result<Ordering> {
         Ok(self.as_slice().cmp(other.as_slice()))
     }
 }
 
-impl From<&[u8; 1]> for ByteVec {
-    fn from(byte: &[u8; 1]) -> ByteVec {
+impl From<&[u8; 1]> for Bytes {
+    fn from(byte: &[u8; 1]) -> Bytes {
         Self {
             length: Length::ONE,
-            inner: vec![byte[0]],
+            inner: Box::new([byte[0]]),
         }
     }
 }
 
-impl From<StrSlice<'_>> for ByteVec {
-    fn from(s: StrSlice<'_>) -> ByteVec {
+impl From<StrSlice<'_>> for Bytes {
+    fn from(s: StrSlice<'_>) -> Bytes {
         let bytes = s.as_bytes();
         debug_assert_eq!(bytes.len(), usize::try_from(s.length).expect("overflow"));
 
-        ByteVec {
-            inner: Vec::from(bytes),
+        Bytes {
+            inner: Box::from(bytes),
             length: s.length,
         }
     }
 }
 
-impl TryFrom<&[u8]> for ByteVec {
+impl TryFrom<&[u8]> for Bytes {
     type Error = Error;
 
     fn try_from(slice: &[u8]) -> Result<Self> {
