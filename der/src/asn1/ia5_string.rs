e2881f870831d658a1ec060802ae4f2646600fa1
Arthur Gautier <arthur.gautier@arista.com>
2022-11-27 22:39:43 -0800

    x509-cert: use Ia5String and use owned types

--- der/src/asn1/ia5_string.rs
+++ der/src/asn1/ia5_string.rs
@@ -1,74 +1,17 @@
 //! ASN.1 `IA5String` support.
 
-use crate::{
-    asn1::AnyRef, ord::OrdIsValueOrd, BytesRef, DecodeValue, EncodeValue, Error, FixedTag, Header,
-    Length, Reader, Result, StrRef, Tag, Writer,
-};
-use core::{fmt, ops::Deref, str};
-
-#[cfg(feature = "alloc")]
-use crate::asn1::Any;
+use crate::{asn1::AnyRef, FixedTag, Result, StrRef, Tag};
+use core::{fmt, ops::Deref};
 
 macro_rules! impl_ia5_string {
     ($type: ty) => {
         impl_ia5_string!($type,);
     };
     ($type: ty, $($li: lifetime)?) => {
-        impl<$($li),*> AsRef<str> for $type {
-            fn as_ref(&self) -> &str {
-                self.as_str()
-            }
-        }
-
-        impl<$($li),*> AsRef<[u8]> for $type {
-            fn as_ref(&self) -> &[u8] {
-                self.as_bytes()
-            }
-        }
-
-        impl<'__der: $($li),*, $($li),*> DecodeValue<'__der> for $type {
-            fn decode_value<R: Reader<'__der>>(reader: &mut R, header: Header) -> Result<Self> {
-                Self::new(BytesRef::decode_value(reader, header)?.as_slice())
-            }
-        }
-
-        impl<$($li),*> EncodeValue for $type {
-            fn value_len(&self) -> Result<Length> {
-                self.inner.value_len()
-            }
-
-            fn encode_value(&self, writer: &mut dyn Writer) -> Result<()> {
-                self.inner.encode_value(writer)
-            }
-        }
+        impl_string_type!($type, $($li),*);
 
         impl<$($li),*> FixedTag for $type {
             const TAG: Tag = Tag::Ia5String;
-        }
-
-        impl<$($li),*> OrdIsValueOrd for $type {}
-
-        impl<'__der: $($li),*, $($li),*> TryFrom<AnyRef<'__der>> for $type {
-            type Error = Error;
-
-            fn try_from(any: AnyRef<'__der>) -> Result<$type> {
-                any.decode_into()
-            }
-        }
-
-        #[cfg(feature = "alloc")]
-        impl<'__der: $($li),*, $($li),*> TryFrom<&'__der Any> for $type {
-            type Error = Error;
-
-            fn try_from(any: &'__der Any) -> Result<$type> {
-                any.decode_into()
-            }
-        }
-
-        impl<$($li),*> fmt::Display for $type {
-            fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-                f.write_str(self.as_str())
-            }
         }
 
         impl<$($li),*> fmt::Debug for $type {
@@ -145,13 +88,11 @@ pub use self::allocation::Ia5String;
 mod allocation {
     use super::Ia5StringRef;
     use crate::{
-        asn1::{Any, AnyRef},
-        ord::OrdIsValueOrd,
+        asn1::AnyRef,
         referenced::{OwnedToRef, RefToOwned},
-        BytesRef, DecodeValue, EncodeValue, Error, FixedTag, Header, Length, Reader, Result,
-        StrOwned, Tag, Writer,
+        FixedTag, Result, StrOwned, Tag,
     };
-    use core::{fmt, ops::Deref, str};
+    use core::{fmt, ops::Deref};
 
     /// ASN.1 `IA5String` type.
     ///
