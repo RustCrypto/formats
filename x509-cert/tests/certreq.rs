0398d5a8edfeeda188c3526a271b04d1080786a6
Arthur Gautier <arthur.gautier@arista.com>
2022-11-27 21:30:01 -0800

    x509-cert: Make extensions owned

--- x509-cert/tests/certreq.rs
+++ x509-cert/tests/certreq.rs
@@ -68,7 +68,7 @@ fn decode_rsa_2048_der() {
         attribute.values.get(0).unwrap().decode_into().unwrap();
     for (ext, (oid, val)) in extensions.iter().zip(EXTENSIONS) {
         assert_eq!(ext.extn_id, oid.parse().unwrap());
-        assert_eq!(ext.extn_value, *val);
+        assert_eq!(ext.extn_value.as_bytes(), *val);
         assert!(!ext.critical);
     }
 
