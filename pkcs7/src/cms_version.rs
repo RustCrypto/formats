ef41b305fddfd912ccee458efe3677fcf823ff65
Simon Dieterle <simon@prodimpct.eu>
2022-12-20 19:35:38 +0100

    Rename CMSVersion to CmsVersion

--- pkcs7/src/cms_version.rs
+++ pkcs7/src/cms_version.rs
@@ -11,7 +11,7 @@ use der::{FixedTag, Tag, DecodeValue, EncodeValue, Length, Writer, Reader, Heade
 ///
 /// See [RFC 5652 10.2.5](https://datatracker.ietf.org/doc/html/rfc5652#section-10.2.5).
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
-pub enum CMSVersion {
+pub enum CmsVersion {
     V0 = 0,
     V1 = 1,
     V2 = 2,
@@ -20,38 +20,38 @@ pub enum CMSVersion {
     V5 = 5
 }
 
-impl FixedTag for CMSVersion {
+impl FixedTag for CmsVersion {
     const TAG: Tag = Tag::Integer;
 }
 
-impl From<CMSVersion> for u8 {
-    fn from(version: CMSVersion) -> Self {
+impl From<CmsVersion> for u8 {
+    fn from(version: CmsVersion) -> Self {
         version as u8
     }
 }
 
-impl TryFrom<u8> for CMSVersion {
+impl TryFrom<u8> for CmsVersion {
     type Error = der::Error;
-    fn try_from(byte: u8) -> der::Result<CMSVersion> {
+    fn try_from(byte: u8) -> der::Result<CmsVersion> {
         match byte {
-            0 => Ok(CMSVersion::V0),
-            1 => Ok(CMSVersion::V1),
-            2 => Ok(CMSVersion::V2),
-            3 => Ok(CMSVersion::V3),
-            4 => Ok(CMSVersion::V4),
-            5 => Ok(CMSVersion::V5),
+            0 => Ok(CmsVersion::V0),
+            1 => Ok(CmsVersion::V1),
+            2 => Ok(CmsVersion::V2),
+            3 => Ok(CmsVersion::V3),
+            4 => Ok(CmsVersion::V4),
+            5 => Ok(CmsVersion::V5),
             _ => Err(Self::TAG.value_error()),
         }
     }
 }
 
-impl<'a> DecodeValue<'a> for CMSVersion {
-    fn decode_value<R: Reader<'a>>(reader: &mut R, header: Header) -> der::Result<CMSVersion> {
-        CMSVersion::try_from(u8::decode_value(reader, header)?)
+impl<'a> DecodeValue<'a> for CmsVersion {
+    fn decode_value<R: Reader<'a>>(reader: &mut R, header: Header) -> der::Result<CmsVersion> {
+        CmsVersion::try_from(u8::decode_value(reader, header)?)
     }
 }
 
-impl EncodeValue for CMSVersion {
+impl EncodeValue for CmsVersion {
     fn value_len(&self) -> der::Result<Length> {
         u8::from(*self).value_len()
     }
