90f0834869bd9cd67d7a2b6098af8bac294d9788
Simon Dieterle <simon@prodimpct.eu>
2022-12-29 08:42:19 +0000

    use Enumerated macro for CmsVersion

--- pkcs7/src/cms_version.rs
+++ pkcs7/src/cms_version.rs
@@ -1,6 +1,6 @@
 //! `CMSVersion` [RFC 5652 § 10.2.5](https://datatracker.ietf.org/doc/html/rfc5652#section-10.2.5)
 
-use der::{DecodeValue, EncodeValue, FixedTag, Header, Length, Reader, Tag, Writer};
+use der::Enumerated;
 
 /// The CMSVersion type gives a syntax version number, for compatibility
 /// with future revisions of this specification.
@@ -10,7 +10,9 @@ use der::{DecodeValue, EncodeValue, FixedTag, Header, Length, Reader, Tag, Write
 /// ```
 ///
 /// See [RFC 5652 10.2.5](https://datatracker.ietf.org/doc/html/rfc5652#section-10.2.5).
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+#[derive(Clone, Copy, Debug, Enumerated, Eq, PartialEq)]
+#[asn1(type = "INTEGER")]
+#[repr(u8)]
 pub enum CmsVersion {
     /// syntax version 0
     V0 = 0,
@@ -24,45 +26,4 @@ pub enum CmsVersion {
     V4 = 4,
     /// syntax version 5
     V5 = 5,
-}
-
-impl FixedTag for CmsVersion {
-    const TAG: Tag = Tag::Integer;
-}
-
-impl From<CmsVersion> for u8 {
-    fn from(version: CmsVersion) -> Self {
-        version as u8
-    }
-}
-
-impl TryFrom<u8> for CmsVersion {
-    type Error = der::Error;
-    fn try_from(byte: u8) -> der::Result<CmsVersion> {
-        match byte {
-            0 => Ok(CmsVersion::V0),
-            1 => Ok(CmsVersion::V1),
-            2 => Ok(CmsVersion::V2),
-            3 => Ok(CmsVersion::V3),
-            4 => Ok(CmsVersion::V4),
-            5 => Ok(CmsVersion::V5),
-            _ => Err(Self::TAG.value_error()),
-        }
-    }
-}
-
-impl<'a> DecodeValue<'a> for CmsVersion {
-    fn decode_value<R: Reader<'a>>(reader: &mut R, header: Header) -> der::Result<CmsVersion> {
-        CmsVersion::try_from(u8::decode_value(reader, header)?)
-    }
-}
-
-impl EncodeValue for CmsVersion {
-    fn value_len(&self) -> der::Result<Length> {
-        u8::from(*self).value_len()
-    }
-
-    fn encode_value(&self, writer: &mut dyn Writer) -> der::Result<()> {
-        u8::from(*self).encode_value(writer)
-    }
 }
