986c825e620bd8e5e7180edc858f17385e89bc23
Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
2023-04-17 20:30:43 +0300

    x509-cert: builder: correct Error type in the comment
    Correct the comment for builder::Error::Signature: it should mention
    signature::Error instead of siganture::Signer.
    
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>

--- x509-cert/src/builder.rs
+++ x509-cert/src/builder.rs
@@ -32,7 +32,7 @@ pub enum Error {
     /// Public key errors propagated from the [`spki::Error`] type.
     PublicKey(spki::Error),
 
-    /// Signing error propagated for the [`signature::Signer`] type.
+    /// Signing error propagated for the [`signature::Error`] or type.
     Signature(signature::Error),
 }
 
