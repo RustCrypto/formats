2bff011f2183151ad1bb562ddfc3544a868cfa48
Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
2023-04-18 03:41:38 +0300

    x509-cert: builder: support signing with RandomizedSigner
    Add support for using RandomizedSigner for signing certificates in
    addition to just Signer keys.
    
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>

--- x509-cert/tests/builder.rs
+++ x509-cert/tests/builder.rs
@@ -129,11 +129,68 @@ fn leaf_certificate() {
     zlint::check_certificate(pem.as_bytes(), ignored);
 }
 
+#[test]
+fn pss_certificate() {
+    let serial_number = SerialNumber::from(42u32);
+    let validity = Validity::from_now(Duration::new(5, 0)).unwrap();
+
+    let issuer =
+        Name::from_str("CN=World domination corporation,O=World domination Inc,C=US").unwrap();
+    let profile = Profile::Leaf {
+        issuer,
+        enable_key_agreement: false,
+        enable_key_encipherment: false,
+    };
+
+    let subject = Name::from_str("CN=service.domination.world").unwrap();
+    let pub_key =
+        SubjectPublicKeyInfoOwned::try_from(RSA_2048_DER_EXAMPLE).expect("get rsa pub key");
+
+    let signer = rsa_pss_signer();
+    let builder =
+        CertificateBuilder::new(profile, serial_number, validity, subject, pub_key, &signer)
+            .expect("Create certificate");
+
+    let certificate = builder
+        .build_with_rng::<rsa::pss::Signature>(&mut rand::thread_rng())
+        .unwrap();
+
+    let pem = certificate.to_pem(LineEnding::LF).expect("generate pem");
+    println!("{}", openssl::check_certificate(pem.as_bytes()));
+
+    // TODO(baloo): not too sure we should tackle those in this API.
+    let ignored = &[
+        "e_sub_cert_aia_missing",
+        "e_sub_cert_crl_distribution_points_missing",
+        "w_sub_cert_aia_does_not_contain_issuing_ca_url",
+        // Missing policies
+        "e_sub_cert_certificate_policies_missing",
+        "e_sub_cert_cert_policy_empty",
+        // Needs to be added by the end-user
+        "e_sub_cert_aia_does_not_contain_ocsp_url",
+        // SAN needs to include DNS name (if used)
+        "e_ext_san_missing",
+        "e_subject_common_name_not_exactly_from_san",
+        // Extended key usage needs to be added by end-user and is use-case dependent
+        "e_sub_cert_eku_missing",
+        // zlint warns on RSAPSS signature algorithms
+        "e_signature_algorithm_not_supported",
+    ];
+
+    zlint::check_certificate(pem.as_bytes(), ignored);
+}
+
 const RSA_2048_PRIV_DER_EXAMPLE: &[u8] = include_bytes!("examples/rsa2048-priv.der");
 
 fn rsa_signer() -> SigningKey<Sha256> {
     let private_key = rsa::RsaPrivateKey::from_pkcs1_der(RSA_2048_PRIV_DER_EXAMPLE).unwrap();
     let signing_key = SigningKey::<Sha256>::new_with_prefix(private_key);
+    signing_key
+}
+
+fn rsa_pss_signer() -> rsa::pss::SigningKey<Sha256> {
+    let private_key = rsa::RsaPrivateKey::from_pkcs1_der(RSA_2048_PRIV_DER_EXAMPLE).unwrap();
+    let signing_key = rsa::pss::SigningKey::<Sha256>::new(private_key);
     signing_key
 }
 
