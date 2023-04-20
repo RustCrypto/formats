f68ebfb774c971263c9d913f23f7c511c4b718f0
Arthur Gautier <arthur.gautier@arista.com>
2023-04-20 08:43:00 -0700

    x509-cert: extraneous PhantomData in `TbsCertificate`

--- x509-cert/src/builder.rs
+++ x509-cert/src/builder.rs
@@ -280,8 +280,6 @@ where
             // https://datatracker.ietf.org/doc/html/rfc5280#section-4.1.2.8
             issuer_unique_id: None,
             subject_unique_id: None,
-
-            _profile: Default::default(),
         };
 
         let extensions = profile.build_extensions(
