5352740b903d6b67ecd5b58fdba1eb7ea27aa8b1
Bernd Krietenstein <bernd.krietenstein@rohde-schwarz.com>
2023-05-24 12:21:02 +0200

    Fixed test for cms signed data builder

--- cms/tests/builder.rs
+++ cms/tests/builder.rs
@@ -1,26 +1,32 @@
 // TODO NM #![cfg(feature = "builder")]
 
-use cms::builder::{create_signing_time_attribute, SignedDataBuilder};
+use p256::{pkcs8::DecodePrivateKey, NistP256};
+use cms::builder::{create_signing_time_attribute, SignedDataBuilder, SignerInfoBuilder};
 use cms::cert::{CertificateChoices, IssuerAndSerialNumber};
-use cms::content_info::CmsVersion;
-use cms::signed_data::{EncapsulatedContentInfo, SignerIdentifier, SignerInfo};
+use cms::signed_data::{EncapsulatedContentInfo, SignerIdentifier};
 use der::asn1::{OctetString, SetOfVec, Utf8StringRef};
 use der::{Any, DecodePem, Encode, Tag, Tagged};
 use pem_rfc7468::LineEnding;
 use rsa::pkcs1::DecodeRsaPrivateKey;
 use rsa::pkcs1v15::SigningKey;
 use sha2::Sha256;
-use spki::{AlgorithmIdentifier, AlgorithmIdentifierOwned};
+use spki::AlgorithmIdentifierOwned;
 use x509_cert::attr::{Attribute, AttributeTypeAndValue};
 use x509_cert::name::{RdnSequence, RelativeDistinguishedName};
 use x509_cert::serial_number::SerialNumber;
 
 const RSA_2048_PRIV_DER_EXAMPLE: &[u8] = include_bytes!("examples/rsa2048-priv.der");
+const PKCS8_PRIVATE_KEY_DER: &[u8] = include_bytes!("examples/p256-priv.der");
 
 fn rsa_signer() -> SigningKey<Sha256> {
     let private_key = rsa::RsaPrivateKey::from_pkcs1_der(RSA_2048_PRIV_DER_EXAMPLE).unwrap();
     let signing_key = SigningKey::<Sha256>::new(private_key);
     signing_key
+}
+
+fn ecdsa_signer() -> ecdsa::SigningKey<NistP256> {
+    let secret_key = p256::SecretKey::from_pkcs8_der(PKCS8_PRIVATE_KEY_DER).unwrap();
+    ecdsa::SigningKey::from(secret_key)
 }
 
 fn signer_identifier(id: i32) -> SignerIdentifier {
@@ -42,11 +48,7 @@ fn signer_identifier(id: i32) -> SignerIdentifier {
 
 #[test]
 fn build_signed_data() {
-    let mut builder = SignedDataBuilder::new();
-    let digest_algorithm = AlgorithmIdentifierOwned {
-        oid: const_oid::db::rfc5912::ID_SHA_256,
-        parameters: None,
-    };
+    // Make some content
     let content = EncapsulatedContentInfo {
         econtent_type: const_oid::db::rfc5911::ID_DATA,
         econtent: Some(
@@ -57,31 +59,61 @@ fn build_signed_data() {
             .unwrap(),
         ),
     };
-    let certificate_buf = include_bytes!("examples/ValidCertificatePathTest1EE.pem");
-    let certificate = x509_cert::Certificate::from_pem(certificate_buf).unwrap();
-    let mut signer = rsa_signer();
-    let signer_info = SignerInfo {
-        version: CmsVersion::V1, // sid: IssuerAndSerialNumber -> V1, subjectKeyIdentifier -> V3
-        sid: signer_identifier(1),
-        digest_alg: digest_algorithm.clone(),
-        signed_attrs: None,
-        signature_algorithm: AlgorithmIdentifier {
-            oid: const_oid::db::rfc5912::SHA_256_WITH_RSA_ENCRYPTION,
-            parameters: Some(Any::null()),
-        },
-        signature: OctetString::new(Vec::new()).unwrap(), // will be set during signing
-        unsigned_attrs: None,
+    // Create multiple signer infos
+    let signer = rsa_signer();
+    let digest_algorithm = AlgorithmIdentifierOwned {
+        oid: const_oid::db::rfc5912::ID_SHA_256,
+        parameters: None,
+    };
+    let signature_algorithm = AlgorithmIdentifierOwned {
+        oid: const_oid::db::rfc5912::SHA_256_WITH_RSA_ENCRYPTION,
+        parameters: Some(Any::null()),
     };
     let external_message_digest = None;
+    let signer_info_builder_1 = SignerInfoBuilder::new(
+        &signer,
+        signer_identifier(1),
+        digest_algorithm.clone(),
+        signature_algorithm,
+        &content,
+        external_message_digest,
+    ).expect("Could not create RSA SignerInfoBuilder");
+
+    let signer_2 = ecdsa_signer();
+    let digest_algorithm_2 = AlgorithmIdentifierOwned {
+        oid: const_oid::db::rfc5912::ID_SHA_512,
+        parameters: None,
+    };
+    // We use ECDSA with NIST P256, which implies SHA-256 for hashing
+    // (according to https://www.rfc-editor.org/rfc/rfc5656#section-6.2.1)
+    let signature_algorithm_2 = AlgorithmIdentifierOwned {
+        oid: const_oid::db::rfc5912::ECDSA_WITH_SHA_256,
+        parameters: Some(Any::null()),
+    };
+    let external_message_digest_2 = None;
+    let signer_info_builder_2 = SignerInfoBuilder::new(
+        &signer_2,
+        signer_identifier(1),
+        digest_algorithm_2.clone(),
+        signature_algorithm_2,
+        &content,
+        external_message_digest_2,
+    ).expect("Could not create ECDSA SignerInfoBuilder");
+
+    let certificate_buf = include_bytes!("examples/ValidCertificatePathTest1EE.pem");
+    let certificate = x509_cert::Certificate::from_pem(certificate_buf).unwrap();
+
+    let mut builder = SignedDataBuilder::new(&content);
+
     let signed_data_pkcs7 = builder
         .add_digest_algorithm(digest_algorithm)
         .expect("could not add a digest algorithm")
-        .set_content_info(content)
-        .expect("adding content failed")
         .add_certificate(CertificateChoices::Certificate(certificate))
         .expect("error adding certificate")
-        .sign(&mut signer, signer_info, external_message_digest)
-        .expect("signing signed data failed")
+        .add_signer_info::<SigningKey<Sha256>, rsa::pkcs1v15::Signature>(signer_info_builder_1)
+        .expect("error adding RSA signer info")
+        .add_signer_info::<ecdsa::SigningKey<NistP256>, p256::ecdsa::DerSignature>(signer_info_builder_2)
+        .expect("error adding RSA signer info")
         .build()
         .expect("building signed data failed");
     let signed_data_pkcs7_der = signed_data_pkcs7
@@ -93,6 +125,13 @@ fn build_signed_data() {
             .expect("PEM encoding of signed data DER failed")
     );
 }
+
+// TODO more tests:
+// - external message
+// - PKCS #7 message:
+//   - different encapsulated content ASN.1 encoding
+//   - enveloped data content
+//   - additional signed attributes
 
 #[test]
 fn test_create_signing_attribute() {
