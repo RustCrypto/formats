8e7d9c7b183ff34bf1b87ab4943afa1036ff7f4e
Arthur Gautier <arthur.gautier@arista.com>
2024-01-04 20:50:50 -0800

    x509-cert: rework the profile of builder
    This is now providing a trait to be implemented by the consumer.
    
    A number of implementation are available, including ones trying to abide
    by CABF Baseline Requirements.
    
    Fixes #1281

---
+++ x509-cert/src/builder/profile/cabf.rs
@@ -0,0 +1,213 @@
+//! CA/Browser forum specific profiles
+//!
+//! <https://cabforum.org/wp-content/uploads/CA-Browser-Forum-BR-v2.0.1.pdf>
+use alloc::vec;
+use std::collections::HashSet;
+
+use crate::{
+    builder::{Error, Profile, Result},
+    certificate::TbsCertificate,
+    ext::{
+        pkix::{
+            AuthorityKeyIdentifier, BasicConstraints, KeyUsage, KeyUsages, SubjectKeyIdentifier,
+        },
+        AsExtension, Extension,
+    },
+    name::Name,
+};
+use const_oid::db::{rfc2256, rfc4519};
+use spki::SubjectPublicKeyInfoRef;
+
+/// Check Name encoding
+///
+/// BR 7.1.4.1 Name Encoding
+pub fn check_names_encoding(name: &Name, multiple_allowed: bool) -> Result<()> {
+    // NOTE: RDNSequence may be empty (at least with tls Subscribers).
+
+    let ordering = vec![
+        rfc4519::DOMAIN_COMPONENT,
+        rfc4519::COUNTRY_NAME,
+        rfc2256::STATE_OR_PROVINCE_NAME,
+        rfc4519::LOCALITY_NAME,
+        rfc4519::POSTAL_CODE,
+        rfc2256::STREET_ADDRESS,
+        rfc4519::ORGANIZATION_NAME,
+        rfc4519::SURNAME,
+        rfc4519::GIVEN_NAME,
+        rfc4519::ORGANIZATIONAL_UNIT_NAME,
+        rfc4519::COMMON_NAME,
+    ];
+    let mut ordering = ordering.iter();
+
+    let mut seen = HashSet::new();
+
+    for rdn in name.0.iter() {
+        if rdn.0.len() != 1 {
+            return Err(Error::NonUniqueRdn);
+        }
+
+        for atv in rdn.0.iter() {
+            if !multiple_allowed && !seen.insert(atv.oid) {
+                return Err(Error::NonUniqueATV);
+            }
+
+            if !ordering.any(|attr| attr == &atv.oid) {
+                return Err(Error::InvalidAttribute { oid: atv.oid });
+            }
+        }
+    }
+
+    Ok(())
+}
+
+/// Check CA subject naming
+///
+/// BR 7.1.2.10.2 CA Certificate Naming
+pub fn ca_certificate_naming(subject: &Name) -> Result<()> {
+    let mut required = HashSet::from([
+        rfc4519::COUNTRY_NAME,
+        rfc4519::ORGANIZATION_NAME,
+        rfc4519::COMMON_NAME,
+    ]);
+    let mut allowed = HashSet::from([
+        rfc4519::COUNTRY_NAME,
+        rfc2256::STATE_OR_PROVINCE_NAME,
+        rfc4519::LOCALITY_NAME,
+        rfc4519::POSTAL_CODE,
+        rfc2256::STREET_ADDRESS,
+        rfc4519::ORGANIZATION_NAME,
+        rfc4519::COMMON_NAME,
+    ]);
+
+    check_names_encoding(subject, false)?;
+
+    for rdn in subject.0.iter() {
+        for atv in rdn.0.iter() {
+            if !allowed.remove(&atv.oid) {
+                return Err(Error::InvalidAttribute { oid: atv.oid });
+            }
+            required.remove(&atv.oid);
+        }
+    }
+
+    if !required.is_empty() {
+        return Err(Error::MissingAttributes);
+    }
+
+    Ok(())
+}
+
+/// Root CA certificate profile
+///
+/// Certificate profile conforming - to the extent possible - to the CABF BR for Root CAs.
+pub struct Root {
+    /// Whether the root CA will emit OCSP responses.
+    /// This adds the [`KeyUsages::DigitalSignature`] bit to the [`KeyUsage`] extension.
+    pub emits_ocsp_response: bool,
+    subject: Name,
+}
+
+impl Root {
+    /// Create a new root profile.
+    pub fn new(emits_ocsp_response: bool, subject: Name) -> Result<Self> {
+        ca_certificate_naming(&subject)?;
+
+        Ok(Self {
+            emits_ocsp_response,
+            subject,
+        })
+    }
+}
+
+impl Profile for Root {
+    fn get_issuer(&self, subject: &Name) -> Name {
+        subject.clone()
+    }
+
+    fn get_subject(&self) -> Name {
+        self.subject.clone()
+    }
+
+    fn build_extensions(
+        &self,
+        spk: SubjectPublicKeyInfoRef<'_>,
+        _issuer_spk: SubjectPublicKeyInfoRef<'_>,
+        tbs: &TbsCertificate,
+    ) -> Result<vec::Vec<Extension>> {
+        let mut extensions: vec::Vec<Extension> = vec::Vec::new();
+
+        // 7.1.2.1.2 Root CA Extensions
+
+        let ski = SubjectKeyIdentifier::try_from(spk)?;
+
+        // ## authorityKeyIdentifier RECOMMENDED
+        // 7.1.2.1.3 Root CA Authority Key Identifier
+        extensions.push(
+            AuthorityKeyIdentifier {
+                // KeyIdentifier must be the same as subjectKeyIdentifier
+                key_identifier: Some(ski.0.clone()),
+                // other fields must not be present.
+                ..Default::default()
+            }
+            .to_extension(&tbs.subject, &extensions)?,
+        );
+
+        // ## basicConstraints MUST
+        // Spec: 7.1.2.1.4 Root CA Basic Constraints
+        extensions.push(
+            BasicConstraints {
+                ca: true,
+                path_len_constraint: None,
+            }
+            .to_extension(&tbs.subject, &extensions)?,
+        );
+
+        // ## keyUsage MUST
+        // Spec: 7.1.2.10.7 CA Certificate Key Usage
+        let mut key_usage = KeyUsages::KeyCertSign | KeyUsages::CRLSign;
+        if self.emits_ocsp_response {
+            key_usage |= KeyUsages::DigitalSignature;
+        }
+        extensions.push(KeyUsage(key_usage).to_extension(&tbs.subject, &extensions)?);
+
+        // ## subjectKeyIdentifier MUST
+        //
+        // TODO: from 7.1.2.11.4 Subject Key Identifier
+        // The CA MUST generate a subjectKeyIdentifier that is unique within the scope of all
+        // Certificates it has issued for each unique public key (the subjectPublicKeyInfo field of the
+        // tbsCertificate). For example, CAs may generate the subject key identifier using an algorithm
+        // derived from the public key, or may generate a sufficiently‐large unique number, such by using a
+        // CSPRNG.
+        extensions.push(ski.to_extension(&tbs.subject, &extensions)?);
+
+        // ## extKeyUsage MUST NOT
+
+        // ## certificatePolicies NOT RECOMMENDED
+
+        // ## Signed Certificate Timestamp List MAY
+
+        // ## Any other extension NOT RECOMMENDED
+
+        Ok(extensions)
+    }
+
+    // 7.1.2.1 Root CA Certificate Profile
+    // TODO:
+    //   - issuerUniqueID MUST NOT be present
+    //   - subjectUniqueID MUST NOT be present
+    // NOTE(baloo): we never build those?
+    //
+    // 7.1.2.1.1 Root CA Validity
+    // TODO:
+    //   - Minimum 2922 days (approx. 8 years)
+    //   - Max 9132 days (approx. 25 years)
+    //
+    //
+}
+
+pub mod tls;
+
+pub mod codesigning {
+    //! <https://cabforum.org/wp-content/uploads/Baseline-Requirements-for-the-Issuance-and-Management-of-Code-Signing.v2.8.pdf>
+    // TODO
+}
---
+++ x509-cert/src/ext/pkix/name/hardware.rs
@@ -0,0 +1,48 @@
+use crate::ext::pkix::name::OtherName;
+use const_oid::db::rfc5911::ID_ON_HARDWARE_MODULE_NAME;
+use der::{
+    asn1::{ObjectIdentifier, OctetString},
+    Any, Sequence, ValueOrd,
+};
+
+/// HardwareModuleName as defined in [RFC 4108 Section 5].
+///
+/// ```text
+/// HardwareModuleName ::= SEQUENCE {
+///     hwType OBJECT IDENTIFIER,
+///     hwSerialNum OCTET STRING
+/// }
+/// ```
+///
+/// [RFC 4108 Section 5]: https://www.rfc-editor.org/rfc/rfc4108#section-5
+#[derive(Clone, Debug, Eq, PartialEq, Sequence, ValueOrd)]
+#[allow(missing_docs)]
+pub struct HardwareModuleName {
+    pub hw_type: ObjectIdentifier,
+
+    pub hw_serial_num: OctetString,
+}
+
+impl HardwareModuleName {
+    /// Convert from an [`OtherName`] if the other name is of the correct type.
+    ///
+    /// It will return `Ok(None)` if the [`OtherName`] contains another type.
+    pub fn from_other_name(other_name: &OtherName) -> der::Result<Option<Self>> {
+        if other_name.type_id == ID_ON_HARDWARE_MODULE_NAME {
+            other_name.value.decode_as().map(Some)
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+impl TryFrom<&HardwareModuleName> for OtherName {
+    type Error = der::Error;
+
+    fn try_from(hmn: &HardwareModuleName) -> der::Result<Self> {
+        Ok(Self {
+            type_id: ID_ON_HARDWARE_MODULE_NAME,
+            value: Any::encode_from(hmn)?,
+        })
+    }
+}
