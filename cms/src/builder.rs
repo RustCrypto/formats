3052b8148766d9bf80945efc0fa3c589eaa32ef4
Angelos Drossos <2845817+DevelAngel@users.noreply.github.com>
2024-06-04 22:54:29 +0200

    implement `std::error::Error` for `cms` and `pkcs5` crates (#1429)
    Allows coercing to `anyhow::Error` using `?`.
    
    Co-authored-by: Devel Angel <develangel@apx-rust-dev.tuxninja>

--- cms/src/builder.rs
+++ cms/src/builder.rs
@@ -66,6 +66,9 @@ pub enum Error {
     Builder(String),
 }
 
+#[cfg(feature = "std")]
+impl std::error::Error for Error {}
+
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
--- pkcs5/src/error.rs
+++ pkcs5/src/error.rs
@@ -36,6 +36,9 @@ pub enum Error {
     },
 }
 
+#[cfg(feature = "std")]
+impl std::error::Error for Error {}
+
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
