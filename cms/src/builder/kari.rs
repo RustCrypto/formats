8dd5f9f2ad07a610fc0943fc2c15c607c1cf1383
nemynm <180121731+nemynm@users.noreply.github.com>
2024-10-12 14:41:21 -0400

    cms: ecc-kari support - add KeyWrapAlgorithm

--- cms/src/builder/kari.rs
+++ cms/src/builder/kari.rs
@@ -10,7 +10,7 @@
 use super::{AlgorithmIdentifierOwned, UserKeyingMaterial};
 
 use const_oid::ObjectIdentifier;
-use der::{asn1::OctetString, Sequence};
+use der::{asn1::OctetString, Any, Sequence};
 
 /// The `EccCmsSharedInfo` type is defined in [RFC 5753 Section 7.2].
 ///
@@ -92,6 +92,121 @@ impl KeyAgreementAlgorithm {
             Self::SinglePassStdDhSha512Kdf => {
                 const_oid::db::rfc5753::DH_SINGLE_PASS_STD_DH_SHA_512_KDF_SCHEME
             }
+        }
+    }
+}
+
+/// Represents supported key wrap algorithm for ECC - as defined in [RFC 5753 Section 7.1.5].
+///
+/// As per [RFC 5753 Section 8]:
+/// ```text
+///  Implementations that support EnvelopedData with the ephemeral-static
+///  ECDH standard primitive:
+///
+///  - MUST support the dhSinglePass-stdDH-sha256kdf-scheme key
+///    agreement algorithm, the id-aes128-wrap key wrap algorithm, and
+///    the id-aes128-cbc content encryption algorithm; and
+/// - MAY support the dhSinglePass-stdDH-sha1kdf-scheme, dhSinglePass-
+///     stdDH-sha224kdf-scheme, dhSinglePass-stdDH-sha384kdf-scheme, and
+///     dhSinglePass-stdDH-sha512kdf-scheme key agreement algorithms;
+///     the id-alg-CMS3DESwrap, id-aes192-wrap, and id-aes256-wrap key
+///     wrap algorithms; and the des-ede3-cbc, id-aes192-cbc, and id-
+///     aes256-cbc content encryption algorithms; other algorithms MAY
+///     also be supported.
+/// ```
+///
+/// As such the following algorithm are currently supported
+/// - id-aes128-wrap
+/// - id-aes192-wrap - (OPTIONAL)
+/// - id-aes256-wrap - (OPTIONAL)
+///
+/// [RFC 5753 Section 8]: https://datatracker.ietf.org/doc/html/rfc5753#section-8
+/// [RFC 5753 Section 7.1.5]: https://datatracker.ietf.org/doc/html/rfc5753#section-7.1.5
+#[derive(Copy, Clone)]
+pub enum KeyWrapAlgorithm {
+    /// id-aes128-wrap
+    Aes128,
+    /// id-aes192-wrap
+    Aes192,
+    /// id-aes256-wrap
+    Aes256,
+}
+impl KeyWrapAlgorithm {
+    /// Return the Object Identifier (OID) of the algorithm.
+    ///
+    /// OID are defined in [RFC 3565 Section 2.3.2]
+    ///
+    /// [RFC 3565 Section 2.3.2]:
+    /// ```text
+    /// NIST has assigned the following OIDs to define the AES key wrap
+    /// algorithm.
+    ///
+    ///     id-aes128-wrap OBJECT IDENTIFIER ::= { aes 5 }
+    ///     id-aes192-wrap OBJECT IDENTIFIER ::= { aes 25 }
+    ///     id-aes256-wrap OBJECT IDENTIFIER ::= { aes 45 }
+    ///
+    /// In all cases the parameters field MUST be absent.
+    /// ```
+    ///
+    /// [RFC 3565 Section 2.3.2]: https://datatracker.ietf.org/doc/html/rfc3565#section-2.3.2
+    fn oid(&self) -> ObjectIdentifier {
+        match self {
+            Self::Aes128 => const_oid::db::rfc5911::ID_AES_128_WRAP,
+            Self::Aes192 => const_oid::db::rfc5911::ID_AES_192_WRAP,
+            Self::Aes256 => const_oid::db::rfc5911::ID_AES_256_WRAP,
+        }
+    }
+
+    /// Return parameters of the algorithm to be used in the context of `AlgorithmIdentifierOwned`.
+    ///
+    /// It should be absent as defined in [RFC 3565 Section 2.3.2] and per usage in [RFC 5753 Section 7.2].
+    ///
+    /// [RFC 3565 Section 2.3.2]: https://datatracker.ietf.org/doc/html/rfc3565#section-2.3.2
+    /// [RFC 5753 Section 7.2]: https://datatracker.ietf.org/doc/html/rfc5753#section-7.2
+    fn parameters(&self) -> Option<Any> {
+        match self {
+            Self::Aes128 => None,
+            Self::Aes192 => None,
+            Self::Aes256 => None,
+        }
+    }
+
+    /// Return key size of the algorithm in number of bits
+    pub fn key_size_in_bits(&self) -> u32 {
+        match self {
+            Self::Aes128 => 128,
+            Self::Aes192 => 192,
+            Self::Aes256 => 256,
+        }
+    }
+}
+impl From<KeyWrapAlgorithm> for AlgorithmIdentifierOwned {
+    /// Convert a `KeyWrapAlgorithm` to the corresponding `AlgorithmIdentifierOwned`.
+    ///
+    /// Conversion is done as defined in [RFC 3565 Section 2.3.2] and according to [RFC 5753 Section 7.2]:
+    ///
+    /// [RFC 3565 Section 2.3.2]
+    /// ```text
+    /// keyInfo contains the object identifier of the key-encryption
+    /// algorithm (used to wrap the CEK) and associated parameters.  In
+    /// this specification, 3DES wrap has NULL parameters while the AES
+    /// wraps have absent parameters.
+    /// ```
+    ///
+    /// [RFC 5753 Section 73.2]
+    /// ```text
+    /// keyInfo contains the object identifier of the key-encryption
+    /// algorithm (used to wrap the CEK) and associated parameters.  In
+    /// this specification, 3DES wrap has NULL parameters while the AES
+    /// wraps have absent parameters.
+    /// ```
+    ///
+    /// [RFC 3565 Section 2.3.2]: https://datatracker.ietf.org/doc/html/rfc3565#section-2.3.2
+    /// [RFC 5753 Section 7.2]: https://datatracker.ietf.org/doc/html/rfc5753#section-7.2
+    fn from(kw_algo: KeyWrapAlgorithm) -> Self {
+        Self {
+            oid: kw_algo.oid(),
+            parameters: kw_algo.parameters(),
         }
     }
 }
