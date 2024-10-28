cc25132cacb178fb0a78d7992f8dcf52285b4b91
Konrad Kohbrok <konrad.kohbrok@datashrine.de>
2024-10-28 11:34:59 +0100

    implement traits for Box<T>

--- tls_codec/src/primitives.rs
+++ tls_codec/src/primitives.rs
@@ -7,6 +7,7 @@ use crate::{DeserializeBytes, SerializeBytes, U24};
 use super::{Deserialize, Error, Serialize, Size};
 
 use core::marker::PhantomData;
+use std::boxed::Box;
 #[cfg(feature = "std")]
 use std::io::{Read, Write};
 
@@ -369,5 +370,42 @@ impl<T> SerializeBytes for PhantomData<T> {
     #[inline(always)]
     fn tls_serialize(&self) -> Result<Vec<u8>, Error> {
         Ok(vec![])
+    }
+}
+
+impl<T: Size> Size for Box<T> {
+    #[inline(always)]
+    fn tls_serialized_len(&self) -> usize {
+        self.as_ref().tls_serialized_len()
+    }
+}
+
+impl<T: Serialize> Serialize for Box<T> {
+    #[cfg(feature = "std")]
+    #[inline(always)]
+    fn tls_serialize<W: Write>(&self, writer: &mut W) -> Result<usize, Error> {
+        self.as_ref().tls_serialize(writer)
+    }
+}
+
+impl<T: SerializeBytes> SerializeBytes for Box<T> {
+    #[inline(always)]
+    fn tls_serialize(&self) -> Result<Vec<u8>, Error> {
+        self.as_ref().tls_serialize()
+    }
+}
+
+impl<T: Deserialize> Deserialize for Box<T> {
+    #[cfg(feature = "std")]
+    #[inline(always)]
+    fn tls_deserialize<R: Read>(bytes: &mut R) -> Result<Self, Error> {
+        T::tls_deserialize(bytes).map(Box::new)
+    }
+}
+
+impl<T: DeserializeBytes> DeserializeBytes for Box<T> {
+    #[inline(always)]
+    fn tls_deserialize_bytes(bytes: &[u8]) -> Result<(Self, &[u8]), Error> {
+        T::tls_deserialize_bytes(bytes).map(|(v, r)| (Box::new(v), r))
     }
 }
