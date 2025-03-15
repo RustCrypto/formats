f19a0fc64f86ab4708e18eb4d2f1358edd40f99f
dishmaker <141624503+dishmaker@users.noreply.github.com>
2025-03-15 23:25:38 +0100

    fix clippy

--- der/tests/derive.rs
+++ der/tests/derive.rs
@@ -387,7 +387,7 @@ mod sequence {
         assert_eq!(spki.subject_public_key, hex!("A0 A1 A2 A3 A4 A5 A6 A7"));
 
         // decode second
-        let (spki, _) = SubjectPublicKeyInfo::from_der_partial(&remaining).unwrap();
+        let (spki, _) = SubjectPublicKeyInfo::from_der_partial(remaining).unwrap();
         assert_eq!(spki.subject_public_key, hex!("B0 B1 B2 B3 B4 B5 B6 B7"));
     }
 
