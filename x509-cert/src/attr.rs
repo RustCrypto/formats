3ac3dc6a4cf2a949c20af03dc5993b8baf77033c
Arthur Gautier <baloo@superbaloo.net>
2025-04-20 07:20:42 -0700

    x509-cert: impl hash for name (#1764)
    * der: impl `Hash` for `SetOf`
    
    Fixes #1763
    
    * x509-cert: impl `Hash` for `Name`
    
    Fixes #1763

--- x509-cert/src/attr.rs
+++ x509-cert/src/attr.rs
@@ -85,7 +85,7 @@ pub type Attributes = SetOfVec<Attribute>;
 ///
 /// [RFC 5280 Appendix A.1]: https://datatracker.ietf.org/doc/html/rfc5280#appendix-A.1
 #[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
-#[derive(Clone, Debug, Eq, PartialEq, PartialOrd, Ord, Sequence, ValueOrd)]
+#[derive(Clone, Debug, Eq, PartialEq, PartialOrd, Ord, Sequence, ValueOrd, Hash)]
 #[allow(missing_docs)]
 pub struct AttributeTypeAndValue {
     pub oid: AttributeType,
