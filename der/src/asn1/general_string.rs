e37e4a8375eecef55e26c71f0ccf30b8f726e835
dishmaker <141624503+dishmaker@users.noreply.github.com>
2025-04-24 21:28:32 +0200

    der: change GeneralStringRef.__contents to &[u8]

--- der/src/asn1/general_string.rs
+++ der/src/asn1/general_string.rs
@@ -1,13 +1,11 @@
-use crate::{DecodeValue, EncodeValue, FixedTag, Header, Length, Reader, Tag, Writer};
-
-use super::OctetStringRef;
+use crate::{BytesRef, DecodeValue, EncodeValue, FixedTag, Header, Length, Reader, Tag, Writer};
 
 /// This is currently `OctetStringRef` internally, as `GeneralString` is not fully implemented yet
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct GeneralStringRef<'a> {
     /// Raw contents, unchecked
     #[doc(hidden)]
-    pub __contents: OctetStringRef<'a>,
+    pub __contents: &'a [u8],
 }
 impl FixedTag for GeneralStringRef<'_> {
     const TAG: Tag = Tag::GeneralString;
@@ -17,16 +15,16 @@ impl<'a> DecodeValue<'a> for GeneralStringRef<'a> {
 
     fn decode_value<R: Reader<'a>>(reader: &mut R, header: Header) -> Result<Self, Self::Error> {
         Ok(Self {
-            __contents: OctetStringRef::decode_value(reader, header)?,
+            __contents: BytesRef::decode_value(reader, header)?.as_slice(),
         })
     }
 }
 impl EncodeValue for GeneralStringRef<'_> {
     fn value_len(&self) -> crate::Result<Length> {
-        self.__contents.value_len()
+        BytesRef::new(self.__contents)?.value_len()
     }
 
     fn encode_value(&self, encoder: &mut impl Writer) -> crate::Result<()> {
-        self.__contents.encode_value(encoder)
+        BytesRef::new(self.__contents)?.encode_value(encoder)
     }
 }
