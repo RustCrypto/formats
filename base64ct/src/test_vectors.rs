1daa5a0520c61337477bb090424aafc25b311aea
Arthur Gautier <arthur.gautier@arista.com>
2025-05-15 11:57:36 -0700

    base64ct: reformat after edition change

--- base64ct/src/test_vectors.rs
+++ base64ct/src/test_vectors.rs
@@ -20,8 +20,7 @@ pub(crate) const UNPADDED_BIN: &[u8] = &[
 ];
 
 /// Padded multi-line Base64 example (from the `ssh-key` crate's `id_ed25519`)
-pub(crate) const MULTILINE_PADDED_BASE64: &str =
-    "b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW\n\
+pub(crate) const MULTILINE_PADDED_BASE64: &str = "b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW\n\
          QyNTUxOQAAACCzPq7zfqLffKoBDe/eo04kH2XxtSmk9D7RQyf1xUqrYgAAAJgAIAxdACAM\n\
          XQAAAAtzc2gtZWQyNTUxOQAAACCzPq7zfqLffKoBDe/eo04kH2XxtSmk9D7RQyf1xUqrYg\n\
          AAAEC2BsIi0QwW2uFscKTUUXNHLsYX4FxlaSDSblbAj7WR7bM+rvN+ot98qgEN796jTiQf\n\
@@ -42,8 +41,7 @@ pub(crate) const MULTILINE_PADDED_BIN: &[u8] = &[
 ];
 
 /// Unpadded multi-line Base64 example (from the `ssh-key` crate's `id_ecdsa_p256`).
-pub(crate) const MULTILINE_UNPADDED_BASE64: &str =
-    "b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAaAAAABNlY2RzYS\n\
+pub(crate) const MULTILINE_UNPADDED_BASE64: &str = "b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAaAAAABNlY2RzYS\n\
          1zaGEyLW5pc3RwMjU2AAAACG5pc3RwMjU2AAAAQQR8H9hzDOU0V76NkkCY7DZIgw+Sqooj\n\
          Y6xlb91FIfpjE+UR8YkbTp5ar44ULQatFaZqQlfz8FHYTooOL5G6gHBHAAAAsB8RBhUfEQ\n\
          YVAAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBHwf2HMM5TRXvo2S\n\
