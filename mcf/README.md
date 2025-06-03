de40754f80219679f572af6e0bda04d0f3b80df2
Tony Arcieri <bascule@gmail.com>
2025-06-02 18:51:09 -0600

    mcf: Modular Crypt Format implementation
    Minimal implementation of a Modular Crypt Format (MCF) parser.
    
    For more information on the format, see:
    <https://passlib.readthedocs.io/en/stable/modular_crypt_format.html>
    
    Note that MCF is less a specification than an ad hoc set of conventions.

---
+++ mcf/README.md
@@ -0,0 +1,75 @@
+# [RustCrypto]: Modular Crypt Format
+
+[![crate][crate-image]][crate-link]
+[![Docs][docs-image]][docs-link]
+[![Build Status][build-image]][build-link]
+![Apache2/MIT licensed][license-image]
+![Rust Version][rustc-image]
+[![Project Chat][chat-image]][chat-link]
+
+Pure Rust implementation of the Modular Crypt Format (MCF), which is used to store password hashes.
+
+## About
+
+Modular Crypt Format is the name for a bespoke set of formats that take the form `${id}$...`, where
+`{id}` is a short numeric or lower case alphanumeric algorithm identifier optionally containing a
+hyphen character (`-`), followed  by `$` as a delimiter, further followed by an algorithm-specific
+serialization of a password hash, typically  using a variant (often an algorithm-specific variant)
+of Base64.
+
+This algorithm-specific  serialization contains one or more fields `${first}[${second}]...`, where
+each field only uses characters in the regexp range `[A-Za-z0-9./+=,\-]`.
+
+Note that MCF has no official specification describing it, and no central registry of identifiers
+exists, nor are there more specific rules for the format than outlined above. MCF is more of an
+ad hoc idea of how to serialize password hashes rather than a standard.
+
+This crate provides types for working with Modular Crypt Format in as generic a manner as possible.
+
+For more information and history on MCF, see the [PassLib documentation].
+
+### Example (SHA-crypt w\ SHA-512):
+
+```text
+$6$rounds=100000$exn6tVc2j/MZD8uG$BI1Xh8qQSK9J4m14uwy7abn.ctj/TIAzlaVCto0MQrOFIeTXsc1iwzH16XEWo/a7c7Y9eVJvufVzYAs4EsPOy0
+```
+
+## Minimum Supported Rust Version
+
+This crate requires **Rust 1.85** at a minimum.
+
+We may change the MSRV in the future, but it will be accompanied by a minor
+version bump.
+
+## License
+
+Licensed under either of:
+
+ * [Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0)
+ * [MIT license](http://opensource.org/licenses/MIT)
+
+at your option.
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in the work by you, as defined in the Apache-2.0 license, shall be
+dual licensed as above, without any additional terms or conditions.
+
+[//]: # (badges)
+
+[crate-image]: https://img.shields.io/crates/v/mcf
+[crate-link]: https://crates.io/crates/mcf
+[docs-image]: https://docs.rs/mcf/badge.svg
+[docs-link]: https://docs.rs/mcf/
+[license-image]: https://img.shields.io/badge/license-Apache2.0/MIT-blue.svg
+[rustc-image]: https://img.shields.io/badge/rustc-1.85+-blue.svg
+[chat-image]: https://img.shields.io/badge/zulip-join_chat-blue.svg
+[chat-link]: https://rustcrypto.zulipchat.com/#narrow/stream/300570-formats
+[build-image]: https://github.com/RustCrypto/formats/actions/workflows/mcf.yml/badge.svg
+[build-link]: https://github.com/RustCrypto/formats/actions/workflows/mcf.yml
+
+[//]: # (links)
+
+[RustCrypto]: https://github.com/rustcrypto
+[PassLib documentation]: https://passlib.readthedocs.io/en/stable/modular_crypt_format.html
---
+++ mcf/src/lib.rs
@@ -0,0 +1,217 @@
+#![no_std]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![doc = include_str!("../README.md")]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/RustCrypto/media/6ee8e381/logo.svg",
+    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/6ee8e381/logo.svg"
+)]
+#![forbid(unsafe_code)]
+#![warn(
+    clippy::mod_module_files,
+    clippy::unwrap_used,
+    missing_docs,
+    unused_qualifications
+)]
+
+extern crate alloc;
+
+use alloc::string::String;
+use core::{fmt, str};
+
+/// MCF field delimiter: `$`.
+pub const DELIMITER: char = '$';
+
+/// Debug message used in panics when invariants aren't properly held.
+const INVARIANT_MSG: &str = "should be ensured valid by constructor";
+
+/// Modular Crypt Format (MCF) serialized password hash.
+///
+/// Password hashes in this format take the form `${id}$...`, where `{id}` is a short numeric or
+/// alphanumeric algorithm identifier optionally containing a `-`, followed by `$` as a delimiter,
+/// further followed by an algorithm-specific serialization of a password hash, typically
+/// using a variant (often an algorithm-specific variant) of Base64. This algorithm-specific
+/// serialization contains one or more fields `${first}[${second}]...`, where each field only uses
+/// characters in the regexp range `[A-Za-z0-9./+=,\-]`.
+///
+/// Example (SHA-crypt w\ SHA-512):
+///
+/// ```text
+/// $6$rounds=100000$exn6tVc2j/MZD8uG$BI1Xh8qQSK9J4m14uwy7abn.ctj/TIAzlaVCto0MQrOFIeTXsc1iwzH16XEWo/a7c7Y9eVJvufVzYAs4EsPOy0
+/// ```
+pub struct McfHash(String);
+
+impl McfHash {
+    /// Parse the given input string, returning an [`McfHash`] if valid.
+    pub fn new(s: impl Into<String>) -> Result<McfHash> {
+        let s = s.into();
+        validate(&s)?;
+        Ok(Self(s))
+    }
+
+    /// Get the contained string as a `str`.
+    pub fn as_str(&self) -> &str {
+        &self.0
+    }
+
+    /// Get the algorithm identifier for this MCF hash.
+    pub fn id(&self) -> &str {
+        Fields::new(self.as_str())
+            .expect(INVARIANT_MSG)
+            .next()
+            .expect(INVARIANT_MSG)
+    }
+
+    /// Get an iterator over the parts of the password hash as delimited by `$`, excluding the
+    /// initial identifier.
+    pub fn fields(&self) -> Fields {
+        let mut fields = Fields::new(self.as_str()).expect(INVARIANT_MSG);
+
+        // Remove the leading identifier
+        let id = fields.next().expect(INVARIANT_MSG);
+        debug_assert_eq!(id, self.id());
+
+        fields
+    }
+}
+
+impl AsRef<str> for McfHash {
+    fn as_ref(&self) -> &str {
+        self.as_str()
+    }
+}
+
+impl fmt::Display for McfHash {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str(self.as_str())
+    }
+}
+
+impl str::FromStr for McfHash {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        Self::new(s)
+    }
+}
+
+/// Perform validations that the given string is well-formed MCF.
+fn validate(s: &str) -> Result<()> {
+    // Validates the hash begins with a leading `$`
+    let mut fields = Fields::new(s)?;
+
+    // Validate characters in the identifier field
+    let id = fields.next().ok_or(Error {})?;
+    validate_id(id)?;
+
+    // Validate the remaining fields have an appropriate format
+    let mut any = false;
+    for field in fields {
+        any = true;
+        validate_field(field)?;
+    }
+
+    // Must have at least one field.
+    if !any {
+        return Err(Error {});
+    }
+
+    Ok(())
+}
+
+/// Validate the password hash identifier is well-formed.
+///
+/// Allowed characters match the regex: `[a-z0-9\-]`, where the first and last characters do NOT
+/// contain a `-`.
+fn validate_id(id: &str) -> Result<()> {
+    let first = id.chars().next().ok_or(Error {})?;
+    let last = id.chars().last().ok_or(Error {})?;
+
+    for c in [first, last] {
+        match c {
+            'a'..='z' | '0'..='9' => (),
+            _ => return Err(Error {}),
+        }
+    }
+
+    for c in id.chars() {
+        match c {
+            'a'..='z' | '0'..='9' | '-' => (),
+            _ => return Err(Error {}),
+        }
+    }
+
+    Ok(())
+}
+
+/// Validate a field in the password hash is well-formed.
+///
+/// Fields include characters in the regexp range `[A-Za-z0-9./+=,\-]`.
+fn validate_field(field: &str) -> Result<()> {
+    if field.is_empty() {
+        return Err(Error {});
+    }
+
+    for c in field.chars() {
+        match c {
+            'A'..='Z' | 'a'..='z' | '0'..='9' | '.' | '/' | '+' | '=' | ',' | '-' => (),
+            _ => return Err(Error {}),
+        }
+    }
+
+    Ok(())
+}
+
+/// Iterator over the `$`-delimited fields of an MCF hash.
+pub struct Fields<'a>(&'a str);
+
+impl<'a> Fields<'a> {
+    /// Create a new field iterator from an MCF hash, returning an error in the event the hash
+    /// doesn't start with a leading `$` prefix.
+    fn new(s: &'a str) -> Result<Self> {
+        let mut ret = Self(s);
+
+        if ret.next() != Some("") {
+            return Err(Error {});
+        }
+
+        Ok(ret)
+    }
+}
+
+impl<'a> Iterator for Fields<'a> {
+    type Item = &'a str;
+
+    fn next(&mut self) -> Option<&'a str> {
+        if self.0.is_empty() {
+            return None;
+        }
+
+        match self.0.split_once(DELIMITER) {
+            Some((field, rest)) => {
+                self.0 = rest;
+                Some(field)
+            }
+            None => {
+                let ret = self.0;
+                self.0 = "";
+                Some(ret)
+            }
+        }
+    }
+}
+
+/// Result type for `mcf`.
+pub type Result<T> = core::result::Result<T, Error>;
+
+/// Error type.
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+#[non_exhaustive]
+pub struct Error {}
+
+impl core::error::Error for Error {}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str("modular crypt format error")
+    }
+}
