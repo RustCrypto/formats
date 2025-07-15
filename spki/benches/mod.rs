5b5c279d6948769b986e3d751cfb6563c3fb03bf
dishmaker <141624503+dishmaker@users.noreply.github.com>
2025-07-15 14:44:11 +0200

    spki: bench SubjectPublicKeyInfoRef

---
+++ spki/benches/mod.rs
@@ -0,0 +1,24 @@
+#![feature(test)]
+#![cfg(feature = "alloc")]
+
+extern crate test;
+
+use spki::SubjectPublicKeyInfoRef;
+use test::{Bencher, black_box};
+
+/// Ed25519 `SubjectPublicKeyInfo` encoded as ASN.1 DER
+const ED25519_DER_EXAMPLE: &[u8] = include_bytes!("../tests/examples/ed25519-pub.der");
+
+#[bench]
+fn bench_spki_decode_encode(b: &mut Bencher) {
+    let mut buf = [0u8; 256];
+    b.iter(|| {
+        let _pk_encoded = black_box({
+            use der::Encode;
+            let pk = SubjectPublicKeyInfoRef::try_from(ED25519_DER_EXAMPLE).unwrap();
+            pk.encode_to_slice(&mut buf)
+        })
+        .unwrap();
+    });
+    b.bytes = ED25519_DER_EXAMPLE.len() as u64;
+}
